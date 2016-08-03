36a12562720d0e227814a7b8dcc95fffaba91a6d
GaelicGamer <noah.castro.hart@gmail.com>
2016-08-03 00:57:38 -0400

    Removing API key from json file and adding it to config file instead.

--- worker.py
+++ worker.py
@@ -28,6 +28,7 @@ REQUIRED_SETTINGS = (
     'GRID',
     'ACCOUNTS',
     'SCAN_RADIUS',
+    'SLEEP'
 )
 for setting_name in REQUIRED_SETTINGS:
     if not hasattr(config, setting_name):
@@ -186,7 +187,7 @@ class Slave(threading.Thread):
             if self.error_code and self.seen_per_cycle:
                 self.error_code = None
             self.step += 1
-            time.sleep(random.uniform(5, 7))
+            time.sleep(random.uniform(config.SLEEP[0],config.SLEEP[1]))
         session.close()
         if self.seen_per_cycle == 0:
             self.error_code = 'NO POKEMON'
